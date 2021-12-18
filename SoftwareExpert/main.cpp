ca29f8ada1da7e566daaea1252f324f99731b6bc
ys88sem.kim <ys88sem.kim@samsung.com>
2021-12-18 22:07:52 +0900

    add baekjoon1330 src code

---
+++ SoftwareExpert/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int A = 0;
+	int B = 0;
+	scanf("%d %d", &A, &B);
+	
+	if (A > B) {
+		printf(">");
+	}
+	else if (A < B) {
+		printf("<");
+	}
+	else {
+		printf("==");
+	}
+}
