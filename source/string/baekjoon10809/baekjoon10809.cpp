5410a8030bdf3a547437d9fe986a3c7488844cfd
ys88sem.kim <ys88sem.kim@samsung.com>
2022-01-29 22:38:19 +0900

    add baekjoon10809 src code string algorithm

---
+++ source/string/baekjoon10809/baekjoon10809.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+
+// baekjoon 10809 find alphabet 
+int main() {
+	char str[105];
+	int isAlpha[27];
+	int answer[27];
+	scanf("%s", &str);
+	
+	for (int i = 0; i < 26; i++) {
+		isAlpha[i] = (i + 97);
+		answer[i] = -1;
+	}
+
+	for (int i = 0; str[i] != '\0'; i++) {
+		for (int j = 0; j < 26; j++) {
+			if (isAlpha[j] == (int)str[i]) {
+				if (answer[j] == -1) {
+					answer[j] = i;
+				}				
+			}
+		}		
+	}
+
+	for (int i = 0; i < 26; i++) {
+		printf("%d ", answer[i]);
+	}
+
+	return 0;
+}
