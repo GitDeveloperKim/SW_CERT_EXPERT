919e62a3eafd7b01e1c5b6c0e7fd9ca623a24628
e**** <ergla@naver.com>
2023-02-21 13:23:10 +0000

    add baekjoon 3003 킹 퀸 룩 비숍 나이트 폰

---
+++ source/simulation/baekjoon3003.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int chess[] = {1, 1, 2, 2, 2, 8};
+    int king = 1;
+    int queen = 1;
+    int rook = 2;
+    int bishop = 2;
+    int knight = 2;
+    int pawn = 8;
+    
+    scanf("%d %d %d %d %d %d", &king, &queen, &rook, &bishop, &knight, &pawn);
+    
+    printf("%d %d %d %d %d %d", chess[0]-king, chess[1]-queen, chess[2]-rook, chess[3]-bishop, chess[4]-knight, chess[5]-pawn);
+    //printf("%d %d %d %d %d %d", chess[0], chess[1], chess[2], chess[3], chess[4], chess[5]);
+}
