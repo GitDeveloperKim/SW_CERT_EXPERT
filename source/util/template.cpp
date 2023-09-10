16eca69196ad74cb242233551713ec8c9334d883
e**** <ergla@naver.com>
2023-09-10 05:43:52 +0000

    add template code

---
+++ source/util/template.cpp
@@ -0,0 +1,15 @@
+#include <iostream>   // standard input output
+#include <algorithm> // sort 
+#include <cstring>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int T = 0;
+    cin >> T;
+
+}
